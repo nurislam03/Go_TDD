22e9dcdeded190c8fb48ba9dd7eca094048e80bc
Nur Islam <nurislam0333@gmail.com>
2019-03-21 17:18:42 +0600

    work with arrays and slices

---
+++ GoFundamentals/arrays_and_slices/sum_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t *testing.T, got, want []int) {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of tails of", func(t *testing.T) {
+		got := sumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("make the sums of tails of", func(t *testing.T) {
+		got := sumAllTails([]int{}, []int{1, 2, 3})
+		want := []int{0, 5}
+		checkSums(t, got, want)
+	})
+}
